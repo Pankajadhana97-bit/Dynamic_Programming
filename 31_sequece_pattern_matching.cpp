c3af262c20ec4aa775eaddd52eb254cda0a8624e
Pankajadhana97-bit <geekyblindcoder@gmail.com>
2021-06-25 08:36:12 +0530

    Lecture 31

--- 31_sequece_pattern_matching.cpp
+++ 31_sequece_pattern_matching.cpp
@@ -26,3 +26,42 @@ int main(){
 	cout << endl;
 	return 0;
 }
+/**
+ * class Solution {
+    
+    int LCS(int n,int m,string s,string t){
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++)dp[i][0]=false;
+        for(int j=0;j<=m;j++)dp[0][j]=false;
+        
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];  
+    }
+public:
+    bool isSubsequence(string s, string t) {
+    //this problem is belong to longest common subsequence 
+       int n=s.size();
+       int m=t.size();
+        if(LCS(n,m,s,t)==n)return true;
+        return false;   
+    }
+};
+ * 
+ * Leetcode question 392 
+ * Link-> https://leetcode.com/problems/is-subsequence/
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
